9bed0fc59cf27abe38a66339aa15511a491360ad
Richa Singh <richasingh30469@gmail.com>
2024-07-07 15:00:43 +0530

    Varshapro2 commit

---
+++ src/new_pack/Test.java
@@ -0,0 +1,12 @@
+package new_pack;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I am varsha tesing two projects varsha pro2");
+
+	}
+
+}
